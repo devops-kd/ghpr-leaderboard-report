1d08e7aea1489ba1ea2b38b14601c96b41fbc051
karthik21118701 <karthikeyan.dharuman@credera.co.uk>
2024-08-28 12:51:40 +0100

    feat: fet pull request method updated

--- src/utils/fetch_pull_requests.py
+++ src/utils/fetch_pull_requests.py
@@ -24,25 +24,21 @@ def get_last_week_date(num_of_days_ago):
     return datetime.now(timezone.utc) - timedelta(days=int(num_of_days_ago))
 
 
-def get_pull_request_api(repo_name, token, state):
+def get_pull_request_api(repo_name, start_date, github ):
     '''
     This function that performs the send request to the github repo using
     PyGithub api. The retun value of this method will list of 
     pull requests from the repo name passed as a parameter
 
     Parameters:
-    repo_name(str): The name of the repo you want to fetch pull request.
-                    eg., 'devops-kd/ghpr-leaderboard-report'
-    token(str)    : Github Personal Access token for authentication
+    repo(github.Repository.Repository): Object of the github.Repository.Repository
     state(str)    : Status of the pull requests, eg., open | merged | closed
 
-    Return type: PaginatedList of github.PullRequest.PullRequest
+    Return type: PaginatedList of github.Issues.Issue
     '''
-    github = Github(token)
-    repo = github.get_repo(repo_name)
-    pull_requests = repo.get_pulls(state=state, sort='created', direction='desc')
-    return pull_requests
-
+    start_date_str = start_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+    query = f'repo:{repo_name} is:pr updated:>={start_date_str}'
+    return github.search_issues(query)
 
 def fetch_pull_requests(repo_name, token, num_of_days_ago):
     '''
@@ -60,13 +56,20 @@ def fetch_pull_requests(repo_name, token, num_of_days_ago):
     Return type: dict
     '''
     last_week_date = get_last_week_date(num_of_days_ago)
-    summary = {'opened': [], 'merged': [], 'closed': []}
-    pull_requests = get_pull_request_api(repo_name, token, "all")
-    for pr in pull_requests:
-        if pr.is_merged() and pr.merged_at > last_week_date:
-            summary['merged'].append(pr)
-        elif pr.state == 'open' and pr.created_at > last_week_date:
-            summary['opened'].append(pr)
-        elif pr.state == 'closed' and pr.closed_at > last_week_date:
-            summary['closed'].append(pr)
+    github = Github(token)
+    repo = github.get_repo(repo_name)
+    summary = {'opened': [], 'merged': [], 'closed': [], 'updated': []}
+    issues = get_pull_request_api(repo_name, last_week_date, github)
+
+    for issue in issues:
+        if issue.pull_request:
+            pr = repo.get_pull(issue.number)
+            if pr.is_merged() and pr.merged_at > last_week_date:
+                summary['merged'].append(pr)
+            elif pr.state == 'open' and pr.created_at > last_week_date:
+                summary['opened'].append(pr)
+            elif pr.state == 'closed' and pr.closed_at > last_week_date:
+                summary['closed'].append(pr)
+            elif pr.state == 'open' and pr.updated_at > last_week_date:
+                summary['updated'].append(pr)
     return summary

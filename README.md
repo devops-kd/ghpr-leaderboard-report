b19474b6111545ade6e64f400afc1382e19a1863
karthik21118701 <karthikeyan.dharuman@credera.co.uk>
2024-08-28 14:32:31 +0100

    feat: Initial draft tenmplate and method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ghpr-leaderboard-report 
+
+Welcome to the Github Pull Request Leader board Report generator. The sole purpose of this project is to generate a simple user friendly report
--- src/utils/generate_report.py
+++ src/utils/generate_report.py
@@ -6,7 +6,7 @@ import json
 from jinja2 import Environment, FileSystemLoader
 
 
-def generate_report_from_j2_template(summary):
+def generate_json_report_from_j2_template(summary):
     '''
     Ths function loads the jinja template and generates a report with pr data.
 
@@ -23,3 +23,25 @@ def generate_report_from_j2_template(summary):
                                     closed_pr=summary["closed"],
                                     updated_pr=summary["updated"])
     return json.loads(rendered_json)
+
+def generate_html_report_from_j2_template(summary):
+    '''
+    Ths function loads the jinja template and generates a report with pr data.
+
+    Parameter:
+    summary(dict): Processed summary of PR data that has opened, closed and merged
+                   pull requests since the given no. of days ago, usually the previous week.
+
+    Return type: dict
+    '''
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('files/html_template.j2')
+    rendered_html = template.render(open_pr=summary["opened"],
+                                    merged_pr=summary["merged"],
+                                    closed_pr=summary["closed"],
+                                    updated_pr=summary["updated"])
+
+    with open('report.html', 'w', encoding="utf-8") as file:
+        file.write(rendered_html)
+
+    return json.loads(rendered_html)
